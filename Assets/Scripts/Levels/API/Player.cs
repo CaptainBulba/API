ecf16d3b5f9f23cbc7c4e133fd715ad6426f20e8
CaptainBulba <varavaivan1@gmail.com>
2022-10-11 23:00:48 +0300

    added slow apearing when creating player

--- Assets/Scripts/Levels/API/Player.cs
+++ Assets/Scripts/Levels/API/Player.cs
@@ -7,17 +7,33 @@ public class Player : MonoBehaviour
     private Vector2 currentPos;
     private Vector2 targetPos;
 
+    private float activateTime = 3f;
+    private bool activatePlayer;
+    private float currentTime = 0;
+
     private ApiController apiController;
 
     private void Start()
     {
         apiController = FindObjectOfType<ApiController>();
+
+        activatePlayer = true;
     }
 
     private void Update()
     {
         if (Vector3.Distance(currentPos, targetPos) > 0.01f)
             transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+
+
+        if (activatePlayer && currentTime <= activateTime)
+        {
+            currentTime += Time.deltaTime;
+            
+            float value = Mathf.Lerp(0f, 1f, currentTime / activateTime);
+
+            GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, value);
+        }
     }
 
     public void SetTargetPosition(float x, float y)
@@ -28,5 +44,6 @@ public class Player : MonoBehaviour
     public void ActivatePlayer()
     {
         gameObject.SetActive(true);
+        activatePlayer = true;
     }
 }
