9d93eeeff370d0ecab18ee958136cb5507e2ee4b
CaptainBulba <varavaivan1@gmail.com>
2022-10-19 22:00:26 +0300

    walls now showing cords

--- Assets/Scripts/Levels/API/Grid/GridManager.cs
+++ Assets/Scripts/Levels/API/Grid/GridManager.cs
@@ -6,6 +6,7 @@ public class GridManager : MonoBehaviour
 {
     private Grid grid;
     [SerializeField] private Tilemap ground;
+    [SerializeField] private Tilemap walls;
     [SerializeField] private GameObject player;
     [SerializeField] private TextMeshProUGUI cordText;
 
@@ -20,7 +21,7 @@ public class GridManager : MonoBehaviour
         Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
         Vector3Int position = grid.WorldToCell(worldPoint);
 
-        if (ground.HasTile(position))
+        if (ground.HasTile(position) && walls.HasTile(position))
             cordText.text = "Coordinates: " + position.x + ", " + position.y;
     }
 }
