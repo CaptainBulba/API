eec3417b2709fad106414b431bcf4eb247304d55
CaptainBulba <varavaivan1@gmail.com>
2022-11-03 22:12:47 +0200

    removed unnecessary

--- Assets/Scripts/Levels/API/QuestChecks/Level 1/TopSecret.cs
+++ Assets/Scripts/Levels/API/QuestChecks/Level 1/TopSecret.cs
@@ -1,12 +1,5 @@
-using UnityEngine;
-
 public class TopSecret : QuestChecks
 {
-    protected override void Start()
-    {
-        base.Start();
-    }
-
     private void Update()
     {
         if(GetApiController().GetPipeman().GetTokenInput() == GetApiController().GetToken())
